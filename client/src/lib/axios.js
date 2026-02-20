902111e38e5d14406d09b03af563c9f96fe883c7
yash rana <2200520100072@gmail.com>
2026-02-20 15:56:25 +0530

    update url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backendUrl =
-  import.meta.env.VITE_BACKEND_URL || "https://instragram-psi.vercel.app";
+  import.meta.env.VITE_BACKEND_URL || "https://instragram-e0of.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: `${backendUrl}/api/v1`,
